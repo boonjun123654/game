622412c9dd398c501c58643ad11dc3a7a627a5c4
boonjun123654 <boonjunlee@gmail.com>
2025-08-02 16:59:21 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('game_options.html')
+
+@app.route('/game')
+def game():
+    mode = request.args.get('mode', 1)
+    return render_template('game.html', mode=mode)
+
+if __name__ == '__main__':
+    app.run(debug=True)
